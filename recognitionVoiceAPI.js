4e319a81a0ccdc806b9168ef81c048ddf584b82a
Patrick <patrick.eric182@gmail.com>
2023-03-30 20:39:57 -0300

    implementing voice API and text on screen

--- recognitionVoiceAPI.js
+++ recognitionVoiceAPI.js
@@ -1,4 +1,20 @@
+const elementKick = document.getElementById('kick')
+
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br';
 recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e){
+    const kick = e.results[0][0].transcript
+    displayKickOnScreen(kick)
+}
+
+function displayKickOnScreen(kick){
+    elementKick.innerHTML = `
+        <div class="message">Você disse:</div>
+        <span class="box">${kick}</span>
+    `
+}
